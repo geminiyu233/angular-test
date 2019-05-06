6bb971bcab5d505e767266b342e43f46f14436f6
yu.shuang <geminiyu233@163.com>
2019-05-06 18:02:16 +0800

    angular学习例子

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300), // 延迟300毫秒
+      distinctUntilChanged(), // 确保过滤条件变化时发送请求
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+}
